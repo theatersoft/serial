56d29b6035d40f1ba1635db1ccb750c9276df692
Rob Huehn <rhuehn@gmail.com>
2018-08-05 11:36:07 -0700

    update serialport to 6 to fix node 10 build errors

--- src/SerialCommand.js
+++ src/SerialCommand.js
@@ -4,29 +4,30 @@ class SerialCommand extends EventEmitter {
     constructor ({port, speed, delimiter = '\r', raw = false}) {
         super()
         this.q = []
-        const SerialPort = require('serialport')
-        this.serialPort = new SerialPort(port, {
-            baudrate: speed,
-            parser: raw ? SerialPort.parsers.raw : SerialPort.parsers.readline(delimiter)
-        })
-            .on('open', () => this.emit('open'))
-            .on('error', err => this.emit('error', err))
-            .on('data', res => {
-                if (this.q.length) {
-                    const cb = this.q.shift().cb
-                    this.cb = cb
-                    if (cb)
-                        cb(res)
+        const
+            SerialPort = require('serialport'),
+            Readline = require('@serialport/parser-readline'),
+            serialPort = new SerialPort(port, {baudRate: speed})
+                .on('open', () => this.emit('open'))
+                .on('error', err => this.emit('error', err)),
+            parser = (raw ? serialPort : serialPort.pipe(new Readline({delimiter})))
+                .on('data', res => {
+                    if (this.q.length) {
+                        const cb = this.q.shift().cb
+                        this.cb = cb
+                        if (cb)
+                            cb(res)
+                        else
+                            this.emit('error', new Error('Unhandled response: ' + res))
+                    } else if (this.cb)
+                        this.cb(res)
                     else
-                        this.emit('error', new Error('Unhandled response: ' + res))
-                } else if (this.cb)
-                    this.cb(res)
-                else
-                    this.emit('error', new Error('Unexpected response: ' + res))
+                        this.emit('error', new Error('Unexpected response: ' + res))
 
-                if (this.q.length)
-                    this.serialPort.write(this.q[0].cmd)
-            })
+                    if (this.q.length)
+                        this.serialPort.write(this.q[0].cmd)
+                })
+        this.serialPort = serialPort
     }
 
     send (cmd, cb) {
